a17726f5e7e82691ef82d180e4b2789488a94b88
yuhao.su <644208937@qq.com>
2023-06-14 16:56:48 +0800

    feat:protoc-crud

---
+++ protoc-gen-go-crud/internal/authorization/deliveries/role.go
@@ -0,0 +1,31 @@
+package authorizationV1
+
+import (
+	authorizationV1 "github.com/fynntang/protobuf-gen-go/protoc-gen-go-crud/api/v1/authorization"
+	components "github.com/fynntang/protobuf-gen-go/protoc-gen-go-crud/api/v1/components"
+	"github.com/gin-gonic/gin"
+	empty "github.com/golang/protobuf/ptypes/empty"
+)
+
+type RoleService struct {
+}
+
+func (a RoleService) CreateRole(*gin.Context, *authorizationV1.CreateRoleRequest) (*empty.Empty, error) {
+	panic("todo")
+}
+
+func (a RoleService) EnableRole(*gin.Context, *authorizationV1.EnableRoleRequest) (*empty.Empty, error) {
+	panic("todo")
+}
+
+func (a RoleService) GetRole(*gin.Context, *authorizationV1.GetRoleRequest) (*components.Role, error) {
+	panic("todo")
+}
+
+func (a RoleService) ListRoles(*gin.Context, *authorizationV1.ListRolesRequest) (*authorizationV1.Roles, error) {
+	panic("todo")
+}
+
+func (a RoleService) UpdateRole(*gin.Context, *authorizationV1.UpdateRoleRequest) (*empty.Empty, error) {
+	panic("todo")
+}
---
+++ protoc-gen-go-crud/version.go
@@ -0,0 +1,4 @@
+package main
+
+// release is the current protoc-gen-go-crud version.
+const release = "v0.0.1"
